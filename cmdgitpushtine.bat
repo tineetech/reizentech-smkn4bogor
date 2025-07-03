95b6e9b5c6277f0dd883054b5aa7c28a5027168a
zeru-program <justinebogor0609@gmail.com>
2025-07-04 00:07:43 +0700

    tes

--- cmdgitpushtine.bat
+++ cmdgitpushtine.bat
@@ -1,7 +1,10 @@
 @echo off
+echo Memulai proses git push ..
+echo ==========================
 SET /P commit_message="Masukkan pesan commit: "
 git add .
 git commit -m "%commit_message%"
 git push origin justine_dev
 echo.
+echo ==========================
 echo Proses Gitpushtine selesai!
